c4fb94158521fd039f3580e7071b2e2e66d91657
victorcezeh <ezeh_victor@yahoo.com>
2026-01-17 22:09:15 +0100

    Installing necessary libraries for the project

---
+++ extract_data.py
@@ -0,0 +1,4 @@
+import os        # I know I will need this
+import requests  # Already installed and I know I will need it
+import boto3     # This is for interacting with AWS API
+import logging   # This is for logs
