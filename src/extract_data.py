a8c877f2e324e81d7149cd1a5264aef6d79670c6
victorcezeh <ezeh_victor@yahoo.com>
2026-01-18 06:56:45 +0100

    Modified extract_data.py file

--- src/extract_data.py
+++ src/extract_data.py
@@ -1,4 +1,5 @@
-import os        # I know I will need this
-import requests  # Already installed and I know I will need it
-import boto3     # This is for interacting with AWS API
+import os                                         # I know I will need this
+import requests                                   # Already installed and I know I will need it
+import boto3                                      # This is for interacting with AWS API
+from dotenv import load_dotenv                    # I will need this to load env variables
 from logging_config import logging_configuration  # Importing log module
