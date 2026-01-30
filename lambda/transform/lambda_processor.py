2f1e5759a88d94dab43ac1b7ba51e30906221df0
victorcezeh <ezeh_victor@yahoo.com>
2026-01-30 04:49:30 +0100

    Change folder from  lambda/transform/lambda_processor.py to lambda/processor/lambda_processor.py

--- lambda/transform/lambda_processor.py
+++
@@ -1,8 +0,0 @@
-from src.transformer import transform_data
-from src.redshift_writer import write_to_redshift
-
-def lambda_handler(event, context):
-    # read api file from s3
-    # load the file into lambda
-    transformed = transform_data(data)
-    write_to_redshift(transformed)
