cb7da0ed6884fb159d0ba10717dda8c5506cdd49
victorcezeh <ezeh_victor@yahoo.com>
2026-04-29 17:18:59 +0100

    Linting code to match CI check

--- src/config.py
+++ src/config.py
@@ -5,14 +5,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Fetch credentials and other config
-aws_access_key=os.getenv("AWS_ACCESS_KEY")
-aws_secret_access_key_id=os.getenv("AWS_SECRET_ACCESS_KEY_ID")
-bucket_name=os.getenv("AWS_BUCKET_NAME")
-bucket_region=os.getenv("AWS_REGION")
-base_url=os.getenv("BASE_URL")
-end_point=os.getenv("END_POINT")
+aws_access_key = os.getenv("AWS_ACCESS_KEY")
+aws_secret_access_key_id = os.getenv("AWS_SECRET_ACCESS_KEY_ID")
+bucket_name = os.getenv("AWS_BUCKET_NAME")
+bucket_region = os.getenv("AWS_REGION")
+base_url = os.getenv("BASE_URL")
+end_point = os.getenv("END_POINT")
 url = f"{os.getenv('BASE_URL')}{os.getenv('END_POINT')}"
 key = os.getenv("KEY")
 
 # Log file path
-log_file=os.getenv("LOG_FILE_PATH")
+log_file = os.getenv("LOG_FILE_PATH")
--- src/logging_config.py
+++ src/logging_config.py
@@ -1,21 +1,26 @@
 import logging
 from config import log_file
 
+
 def logging_configuration():
 
     if not log_file:
-            print("ERROR: LOG_FILE_PATH is not set or is none!")
-            return logging.getLogger()
+        print("ERROR: LOG_FILE_PATH is not set or is none!")
+        return logging.getLogger()
     try:
-        logging.basicConfig(level=logging.INFO, 
-        format="%(asctime)s:%(levelname)s:%(message)s",
-        handlers=[logging.FileHandler(filename=log_file, 
-        mode="a"), logging.StreamHandler()])
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(asctime)s:%(levelname)s:%(message)s",
+            handlers=[
+                logging.FileHandler(filename=log_file, mode="a"),
+                logging.StreamHandler(),
+            ],
+        )
         logger = logging.getLogger()
         logging.getLogger("urllib3").setLevel(logging.WARNING)
         logging.info("Logging setup complete!")
         return logger
-    
+
     except PermissionError:
         logging.warning(f"Permission denied: Cannot write to {log_file}.")
         return logging.getLogger()
@@ -23,12 +28,13 @@ def logging_configuration():
     except FileNotFoundError:
         logging.warning(f"File path not found: {log_file}")
         return logging.getLogger()
-    
+
     except Exception as e:
         logging.warning(f"ERROR: Logging setup failed to complete {e}")
         logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s")
         logging.warning("Falling back to basic level configuration.")
         return logging.getLogger()
+
 
 if __name__ == "__main__":
     logging_configuration()
